38596bb32e8816b2ab0563ba7e5dcfa1f7c56cf9
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-27 10:40:44 +0500

    1 Program to find Students grades by using if-else statement

---
+++ ifelse.rb
@@ -0,0 +1,15 @@
+#if-else statement with one condition
+#marks = 70
+#if marks>=50 
+ #  puts "Student is pass."
+#else 
+ #  puts "Student is fail."
+#end
+
+#if statement with multiple conditions
+marks = 78
+if marks<=50 && marks>=20 
+     puts "Student is pass." 
+else 
+    puts "Student is fail."  
+end
