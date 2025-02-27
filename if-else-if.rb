3600beec019dcddafcb14b7b949e04bd163e695f
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-27 14:00:59 +0500

    Correction of Students grades marks in program using if-else-if statement

--- if-else-if.rb
+++ if-else-if.rb
@@ -1,39 +1,36 @@
+puts "Enter marks: "
 
+# Directly read numeric input(also negative values)
+begin
+  marks = Integer(gets) 
 
-# if - else - if statement
- 
-marks = 200
+      if   marks < 0 || marks > 200 
+      puts "Invalid marks"
 
+      elsif marks >= 181 && marks <= 200
+      puts "Student gets A+ grade"   
 
-if   marks > 100
-    puts "Invalid marks" 
-
-elsif   marks >= 181 && marks <= 200
-    puts "Student gets A+ grade"   
-
-elsif marks >= 161 && marks <= 180  
-    puts "Student gets A grade"
+      elsif marks >= 161 && marks <= 180  
+      puts "Student gets A grade"
     
-elsif marks >= 151 && marks <= 160  
-    puts "Student gets B grade"
+      elsif marks >= 151 && marks <= 160  
+      puts "Student gets B grade"
 
-elsif marks >= 131 && marks <= 150  
-    puts "Student gets C grade"  
+      elsif marks >= 131 && marks <= 150  
+      puts "Student gets C grade"  
 
-elsif marks >= 101 && marks <= 130  
-    puts "Student gets D grade" 
+      elsif marks >= 101 && marks <= 130  
+      puts "Student gets D grade" 
 
-elsif marks >= 51 && marks <= 100  
-    puts "Student gets E grade" 
+      elsif marks >= 51 && marks <= 100  
+      puts "Student gets E grade" 
 
-elsif marks < 50  
-    puts "Student gets F grade" 
+      # marks <50
+      else  
+      puts "Student gets F grade"
+      end
 
-elsif marks >= 0 && <=200 
-    puts ""
-
-else  
-  puts "Invalid"  
-
-
-end
+     # Handle cases where input will be non numeric values
+    rescue ArgumentError 
+        puts "Invalid input! Please enter a numeric value."
+      end
