e9c32b7c6d66ee27e50752b6978afa940503f273
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-27 10:45:47 +0500

    How we find Students grades by using ternary statement

---
+++ ternary.rb
@@ -0,0 +1,4 @@
+# ternary statement
+marks = 78
+#puts a=(marks>=10 && marks<=50) ? Value is less than 50 : Value is greater than 50 ;
+puts marks>=10 && marks<=50 ? "Value is less than 50" : "Value is greater than 50" 
