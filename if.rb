ee1eedace6a8bca90b6590c1777baba83b7ad913
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-27 10:05:41 +0500

    1 Program to illustrate the Students grades by using if statement

---
+++ if.rb
@@ -0,0 +1,12 @@
+#if statement with one condition
+#marks = 20 
+#if marks<=50 
+ #   puts "Student is pass."
+#rubyb if.rb
+#end
+
+#if statement with multiple conditions
+marks = 35
+if marks<=50 && marks>=20 
+     puts "Student is pass."    
+end
