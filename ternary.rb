842b1624b9b2cda67703cf4b8f7302a4d50c9e1d
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-27 16:30:01 +0500

    Correction in Students grades program using ternary statement

--- ternary.rb
+++ ternary.rb
@@ -1,4 +1,21 @@
-# ternary statement
-marks = 78
-#puts a=(marks>=10 && marks<=50) ? Value is less than 50 : Value is greater than 50 ;
-puts marks>=10 && marks<=50 ? "Value is less than 50" : "Value is greater than 50" 
+# ternary statement which is
+# alternate to if-else statement
+ 
+puts "Enter marks: "
+begin
+  # Get user input
+  marks = Integer(gets) 
+# Check if input is a valid number
+
+grade = marks < 0 || marks > 200 ? "Invalid marks" :
+        marks >= 181 ? "Grade is A+" :
+        marks >= 161 ? "Grade is A" :
+        marks >= 151 ? "Grade is B" :
+        marks >= 131 ? "Grade is C" :
+        marks >= 101 ? "Grade is D" :
+        marks >= 51  ? "Grade is E" : "Grade is F"
+
+puts grade
+rescue ArgumentError
+    puts "Invalid input! Please enter a numeric value."
+  end
