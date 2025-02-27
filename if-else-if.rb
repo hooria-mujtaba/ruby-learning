7bca4cea31fbaddf7172a0bb2bd244d452d0b445
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-27 11:15:05 +0500

    1 Correction according to issue in Students grades program

--- if-else-if.rb
+++ if-else-if.rb
@@ -2,32 +2,35 @@
 
 # if - else - if statement
  
-marks = 75.5
+marks = 200
 
 
 if   marks > 100
     puts "Invalid marks" 
 
-elsif   marks >= 90 && marks <= 100
+elsif   marks >= 181 && marks <= 200
     puts "Student gets A+ grade"   
 
-elsif marks >= 80 && marks <= 90  
+elsif marks >= 161 && marks <= 180  
     puts "Student gets A grade"
     
-elsif marks >= 70 && marks <= 80  
+elsif marks >= 151 && marks <= 160  
     puts "Student gets B grade"
 
-elsif marks >= 60 && marks <= 70  
-    puts "Student gets D grade"  
+elsif marks >= 131 && marks <= 150  
+    puts "Student gets C grade"  
 
-elsif marks >= 50 && marks <= 60  
+elsif marks >= 101 && marks <= 130  
+    puts "Student gets D grade" 
+
+elsif marks >= 51 && marks <= 100  
     puts "Student gets E grade" 
 
-elsif marks < 50 && marks >=0  
-    puts "Student is Fail" 
+elsif marks < 50  
+    puts "Student gets F grade" 
 
-elsif marks < 0  
-    puts "Number is negative"
+elsif marks >= 0 && <=200 
+    puts ""
 
 else  
   puts "Invalid"  
