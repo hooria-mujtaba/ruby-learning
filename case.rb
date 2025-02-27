74363097d8ef1010583de554f153ad4eeb5a965a
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-27 09:41:38 +0500

    1 Calculate How we can find Students grades by using Case Statement

---
+++ case.rb
@@ -0,0 +1,14 @@
+
+# Case statement 
+marks=98
+case marks
+when 90..100
+    puts "Outstanding "
+when 80..90
+    puts "Well Done "
+when 70..80
+    puts "Good"
+else
+    puts "Need Improvement"
+end
+
