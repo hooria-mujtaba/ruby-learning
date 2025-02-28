5821ee40490b01ccc6258af38a8ea26f663cee8f
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-28 16:39:52 +0500

    Correction in Students grades program using if-else-if statement

--- if.rb
+++ if.rb
@@ -1,35 +1,43 @@
-#if statement 
+#Program to illustrate Students marks gardes using if statement 
 
-    begin
-     marks=200  
-     if marks < 0 || marks > 200
-      puts "Marks Invalid"
-     end
-       if marks >=180 && marks <= 200
-      puts "Student gets A+ grades"
-       end  
+puts " Enter marks: "
+
+begin
+  marks=Integer(gets)
+
+      if marks < 0 || marks > 200
+        puts "Marks Invalid"
+      end
+
+      if marks >=180 && marks <= 200
+        puts "Student gets A+ grades"
+      end  
+
       if marks >=161 && marks <= 180
-      puts "Student gets A grades"
+        puts "Student gets A grades"
       end    
+
       if marks >=151 && marks <= 160
-      puts "Student gets B grades"
+        puts "Student gets B grades"
       end   
+
       if marks >=131 && marks <= 150
-      puts "Student gets C grades"
+        puts "Student gets C grades"
       end   
+
       if marks >=101 && marks <= 130
-      puts "Student gets D grades"
+        puts "Student gets D grades"
       end   
+
       if marks >=51 && marks <= 100
-      puts "Student gets E grades"
-      end   
+        puts "Student gets E grades"
+      end 
+
       if marks >=0 && marks < 50
-      puts "Student gets F grades"
+        puts "Student gets F grades"
    end
 
-   # This statement will be executed when exception occurs (user does'n follow the rules)
-   rescue 
-     puts "Invalid input! Please enter a numeric value."
-    end
-    
-     
+#This statement will be executed when exception occurs (user does'n follow the rules)
+rescue 
+  puts "Invalid input! Please enter a numeric value."
+end    
