d8d59716a273b4ffb7bca1d165372f54d861a294
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-02-28 16:57:19 +0500

    Correction in Students grades program using ternary statement

--- ternary.rb
+++ ternary.rb
@@ -1,11 +1,12 @@
-# ternary statement which is
-# alternate to if-else statement
- 
-begin
-  marks = 200
-# Check if input is a valid number
+# ternary statement which is alternate to if-else statement
 
-grade = marks < 0 || marks > 200 ? "Invalid marks" :
+puts " Enter marks: "
+
+begin
+  marks=Integer(gets)
+
+  # Check if input is a valid number
+  grade = marks < 0 || marks > 200 ? "Invalid marks" :
         marks >= 181 ? "Grade is A+" :
         marks >= 161 ? "Grade is A" :
         marks >= 151 ? "Grade is B" :
@@ -13,8 +14,9 @@ grade = marks < 0 || marks > 200 ? "Invalid marks" :
         marks >= 101 ? "Grade is D" :
         marks >= 51  ? "Grade is E" : "Grade is F"
 
-puts grade
-# This statement will be executed when exception occurs (user does'n follow the rules)
+  puts grade
+
+#This statement will be executed when exception occurs (user does'n follow the rules)
 rescue 
-    puts "Invalid input! Please enter a numeric value."
-  end
+   puts "Invalid input! Please enter a numeric value."
+end
