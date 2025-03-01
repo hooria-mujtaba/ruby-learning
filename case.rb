5b06feeb27d9e77685f0f9d12a65a037c6c3beb3
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-03-01 16:41:18 +0500

    Correction in Students grades marks using case statement

--- case.rb
+++ case.rb
@@ -1,30 +1,29 @@
-# Program to illustrate Students marks gardes using case statement. 
-puts " Enter marks: "
+# Program to illustrate Students marks grades using case statement.
+puts "Enter marks: "
 
 begin
-  marks=Integer(gets)
+  marks = Integer(gets.chomp)
 
   case marks
-     when 0..50
-     puts "Grade is F"
-     when 51..100
-     puts "Grade is E"
-     when 101..130
-     puts "Grade is D"
-     when 131..150
-     puts "Grade is C"
-     when 151..160
-     puts "Grade is B"
-     when 161..180
-     puts "Grade is A"
-     when 181..200
-     puts "Grade is A+"
-
-  #This case will be executed when input will not be in range
-   else
-   puts "Invalid marks" 
-   end
-#This statement will be executed when exception occurs (user does'n follow the rules)
-rescue 
+  when 0..50
+    puts "Grade is F"
+  when 51..100
+    puts "Grade is E"
+  when 101..130
+    puts "Grade is D"
+  when 131..150
+    puts "Grade is C"
+  when 151..160
+    puts "Grade is B"
+  when 161..180
+    puts "Grade is A"
+  when 181..200
+    puts "Grade is A+"
+  else
+    # This case will be executed when input is not in range
+    puts "Invalid marks"
+  end
+rescue ArgumentError
+  # This statement will be executed when an exception occurs (user doesn't follow the rules)
   puts "Invalid input! Please enter a numeric value."
 end
