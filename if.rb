2efb28f9eaf2fa4962ff1d6f98b8445525a556ad
Hooria Mujtaba <hooria.livebinders@gmail.com>
2025-03-01 17:05:13 +0500

    Correction in Students grades marks using if statement

--- if.rb
+++ if.rb
@@ -1,43 +1,42 @@
-#Program to illustrate Students marks gardes using if statement 
+#Program to illustrate Students marks gardes using if statement
 
-puts " Enter marks: "
+puts "Enter marks: "
 
 begin
-  marks=Integer(gets)
+  marks = Integer(gets)
 
-      if marks < 0 || marks > 200
-        puts "Marks Invalid"
-      end
+  if marks < 0 || marks > 200
+    puts "Marks Invalid"
+  end
 
-      if marks >=180 && marks <= 200
-        puts "Student gets A+ grades"
-      end  
+  if marks >= 180 && marks <= 200
+    puts "Student gets A+ grades"
+  end
 
-      if marks >=161 && marks <= 180
-        puts "Student gets A grades"
-      end    
+  if marks >= 161 && marks <= 180
+    puts "Student gets A grades"
+  end
 
-      if marks >=151 && marks <= 160
-        puts "Student gets B grades"
-      end   
+  if marks >= 151 && marks <= 160
+    puts "Student gets B grades"
+  end
 
-      if marks >=131 && marks <= 150
-        puts "Student gets C grades"
-      end   
+  if marks >= 131 && marks <= 150
+    puts "Student gets C grades"
+  end
 
-      if marks >=101 && marks <= 130
-        puts "Student gets D grades"
-      end   
+  if marks >= 101 && marks <= 130
+    puts "Student gets D grades"
+  end
 
-      if marks >=51 && marks <= 100
-        puts "Student gets E grades"
-      end 
+  if marks >= 51 && marks <= 100
+    puts "Student gets E grades"
+  end
 
-      if marks >=0 && marks < 50
-        puts "Student gets F grades"
-   end
-
-#This statement will be executed when exception occurs (user does'n follow the rules)
-rescue 
+  if marks >= 0 && marks < 50
+    puts "Student gets F grades"
+  end
+rescue ArgumentError
+  #This statement will be executed when exception occurs (user does'n follow the rules)
   puts "Invalid input! Please enter a numeric value."
-end    
+end
